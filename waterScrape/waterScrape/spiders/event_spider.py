db6612fe77bb3d7936cbe42a9bf4179e81e12fcc
RamseyvanderMeer <ramvandermeer@gmail.com>
2021-02-20 20:29:56 -0800

    final before DB incoperation

--- waterScrape/waterScrape/spiders/event_spider.py
+++ waterScrape/waterScrape/spiders/event_spider.py
@@ -3,16 +3,19 @@ import json
 
 class eventsSpider(scrapy.Spider):
     name = "event"
-    
 
-    start_urls = [
-        'https://www.internationalrafting.com/racing/events/'
-    ]
+    def start_requests(self):
+        urls = [
+            'https://www.internationalrafting.com/racing/events/'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         events = {}
         
         for post in response.css('div.mec-topsec'):
+            #self.logger.info('Parse function called on %s', response.url)
             #yield {
                 #'title': post.css('a.mec-color-hover::text').get(),
                 #'title': post.css('div.mec-event-content h3.mec-event-title a.mec-color-hover::text').get()
@@ -20,7 +23,7 @@ class eventsSpider(scrapy.Spider):
                 #'description': post.css('div.mec-event-description::text').get()
             #}
             event = {}
-            event.update({"date" : post.css('span.mec-start-date-label::text').get(), "description" : post.css('div.mec-event-description::text').get(), "place" : post.css('div.mec-venue-details address span::text').get()})
+            event.update({"date" : post.css('span.mec-start-date-label::text').get(), "description" : post.css('div.mec-event-description::text').get(), "place" : post.css('div.mec-venue-details address span::text').get(), "url" : post.css("a.mec-color-hover::attr('href')").get()})
             events[post.css('a.mec-color-hover::text').get().encode("ascii", "ignore").decode()] = event
             with open('result.json', 'w') as fp:
                 json.dump(events, fp)
